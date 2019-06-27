07cb7de946d0fbe02f25afe0bdccd4d543538be3
livingOnARock <41513380+livingOnARock@users.noreply.github.com>
2019-06-27 12:42:55 -0500

    change class component to functional component

--- src/components/Businesses.js
+++ src/components/Businesses.js
@@ -1,24 +1,22 @@
 import React from "react";
 import Business from "./Business";
 
-class Businesses extends React.Component {
-  render() {
-    return (
-      <div
-        className="BusinessList"
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-evenly",
-          flexWrap: "wrap"
-        }}
-      >
-        {this.props.businesses.map(function(business) {
-          return <Business key={business.id} business={business} />;
-        })}
-      </div>
-    );
-  }
-}
+const Businesses = ({ businesses }) => {
+  return (
+    <div
+      className="BusinessList"
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-evenly",
+        flexWrap: "wrap"
+      }}
+    >
+      {businesses.map(function(business) {
+        return <Business key={business.id} business={business} />;
+      })}
+    </div>
+  );
+};
 
 export default Businesses;
